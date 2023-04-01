a74a1f3164e162b8443a3169f81a76fdc52ab813
TroyFoote <troyfoote@gmail.com>
2023-04-02 01:05:06 +0800

    updated logic.js

--- Leaflet_Part_1/static/js/logic.js
+++ Leaflet_Part_1/static/js/logic.js
@@ -1,9 +1,53 @@
-// Creating the map object
+const PIXELS_PER_MAG = 5;
+
+
+// Conditionals for earthquake magnitude by their size and depth of the earthquake by colour
+function depthColor(depth) {
+
+  let color = "";
+  if (depth > 90) {
+    color = "black";
+  }
+  else if (depth > 70) {
+    color = "purple";
+  }
+  else if (depth > 50) {
+    color = "blue";
+  }
+  else if (depth > 30) {
+   color = "green";
+  }
+ else if (depth > 10) {
+   color = "yellow";
+  }
+ else {
+    color = "white";
+  }
+  
+  return color;
+ }  
+
+function styleInfo(color, radius) {
+  let info = {
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: color,
+    color: "black",
+    radius: radius,
+    stroke: true,
+    weight: 0.5
+  };
+
+return info;
+}
+
+ // Creating the map object
 let myMap = L.map("map", {
     center: [0.0, 0.0],
     zoom: 3
   });
-  
+let majorEarthquakes = new L.LayerGroup();
+
   // Adding the tile layer
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -17,49 +61,32 @@ let myMap = L.map("map", {
   // Get the data with d3.
   d3.json(geoData).then(function(data) {
 
-  L.geojson(data).addTo(myMap)
-  
-  // Loop through the data.
-  for (let i = 0; i < data.length; i++) {
+    L.geoJSON(data).addTo(myMap);
+   console.log(data)
 
-    // Set the data location property to a variable.
-    let location = data[i].location.geometry.coordinates;
-
-    // Check for the location property.
-    if (location) {
-
-      // Add a new marker to the cluster group, and bind a popup.
-      markers.addLayer(L.marker([location.geometry.coordinates[1], location.geometry.coordinates[0]])
-        .bindPopup(data[i].descriptor));
-    }
- // Add our marker cluster layer to the map.
- myMap.addLayer(markers);
-  }
-  
-  console.log(data);
-
-   // Conditionals for earthquake magnitude by their size and depth of the earthquake by colour
-   let color = "";
-   if (location.geometry.controls[2] > 90) {
-     color = "black";
-   }
-   else if (location.geometry.controls[2] > 70) {
-     color = "purple";
-   }
-   else if (location.geometry.controls[2] > 50) {
-     color = "blue";
-   }
-   else if (location.geometry.controls[2] > 30) {
-    color = "green";
-   }
-  else if (location.geometry.controls[2] > 10) {
-    color = "yellow";
-   }
-  else {
-     color = "white";
-   }
- 
-  
+    // Loop through the data.
+    for (let i = 0; i < data.features.length; i++) {
     
+      console.log(data.features[i])
+
+      // Set the data location property to a variable.
+      let coordinates = data.features[i].geometry.coordinates;
+      let properties = data.features[i].properties;
+      
+      // Check for the location property.
+      if (coordinates) {
+        let color = depthColor(coordinates[2]);
+        let radius = properties.mag * PIXELS_PER_MAG;
+        console.log(radius)
+
+        // Add a new marker to the cluster group, and bind a popup.
+        let style = styleInfo(color, radius);
+        majorEarthquakes.addLayer(L.circleMarker([coordinates[1], coordinates[0]], style)
+          .bindPopup(properties.place));
+      }
+  // Add our marker cluster layer to the map.
+  myMap.addLayer(majorEarthquakes);
+    }
   
 });
+
