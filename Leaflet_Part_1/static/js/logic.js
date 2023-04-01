24d993ebe4c1358609c3c44d0a2e186a99428c55
TroyFoote <troyfoote@gmail.com>
2023-04-01 21:56:23 +0800

    made additions to logic.js

--- Leaflet_Part_1/static/js/logic.js
+++ Leaflet_Part_1/static/js/logic.js
@@ -12,32 +12,54 @@ let myMap = L.map("map", {
   // Load the GeoJSON data.
   let geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
   
-  let geojson;
+  // let geojson;
   
   // Get the data with d3.
   d3.json(geoData).then(function(data) {
-  
-    // Create a new marker cluster group.
-  let markers = L.markerClusterGroup();
 
+  L.geojson(data).addTo(myMap)
+  
   // Loop through the data.
   for (let i = 0; i < data.length; i++) {
 
     // Set the data location property to a variable.
-    let location = data[i].location;
+    let location = data[i].location.geometry.coordinates;
 
     // Check for the location property.
     if (location) {
 
       // Add a new marker to the cluster group, and bind a popup.
-      markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
+      markers.addLayer(L.marker([location.geometry.coordinates[1], location.geometry.coordinates[0]])
         .bindPopup(data[i].descriptor));
     }
  // Add our marker cluster layer to the map.
  myMap.addLayer(markers);
   }
   
+  console.log(data);
+
+   // Conditionals for earthquake magnitude by their size and depth of the earthquake by colour
+   let color = "";
+   if (location.geometry.controls[2] > 90) {
+     color = "black";
+   }
+   else if (location.geometry.controls[2] > 70) {
+     color = "purple";
+   }
+   else if (location.geometry.controls[2] > 50) {
+     color = "blue";
+   }
+   else if (location.geometry.controls[2] > 30) {
+    color = "green";
+   }
+  else if (location.geometry.controls[2] > 10) {
+    color = "yellow";
+   }
+  else {
+     color = "white";
+   }
+ 
   
-    console.log(data);
+    
   
 });
